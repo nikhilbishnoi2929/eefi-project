f114b71d69fa84d7ee51b14044688b70bccddd08
Your Name <your_email@example.com>
2024-09-16 14:45:56 +0530

    first commit

---
+++ src/components/Building.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+const Building = () => {
+  return (
+    <div className="bg-hero-image h-screen">
+      <Header />
+      <h2 className=" font-plus_jakarta">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ex
+        corrupti ab deleniti, similique magnam quas dolorem, eum odit atque
+        nobis, ipsam dignissimos accusamus ipsum. Natus adipisci porro eius, sed
+        magnam autem culpa, earum suscipit assumenda aliquam ipsa repellendus
+        est.
+      </h2>
+    </div>
+  );
+};
+
+export default Building;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-image": "url(./assets/images/png/hero-image.png)",
+      },
+    },
+  },
+  plugins: [],
+};
